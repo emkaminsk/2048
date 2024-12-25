f4775e4bb74d2409453f1cbe92a3aa63e4865e61
Marcin Kamiński <emkaminsk@gmail.com>
2024-12-25 22:58:27 +0100

    Flat structure

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, send_file
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html') 
+    return send_file('index.html')
+
+@app.route('/style.css')
+def styles():
+    return send_file('style.css')
+
+@app.route('/game.js')
+def game():
+    return send_file('game.js')
+
+@app.route('/all.min.css')
+def fontawesome_css():
+    return send_file('all.min.css')
+
+@app.route('/<path:filename>')
+def serve_files(filename):
+    return send_file(filename) 
